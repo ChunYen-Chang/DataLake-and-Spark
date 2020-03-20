b579d4ce1700fac74fd0a4da6a7c20b1d6643ebc
kk <kk>
2020-03-21 07:24:08 +0800

    FIRST COMMIT

---
+++ code/ETL_LocaltoCloudDataLake.py
@@ -0,0 +1,42 @@
+# import necessary package
+import ConnectAWS_function
+
+
+def main():
+    """
+    Description: This function helps users make a connection to AWS service, 
+                 create a directory in Cloud data lake(AWS S3), and upload
+                 data from local directory to the directory in Cloud data lake
+    Parameters: None
+    Return: None
+    """
+    # settings
+    bucket_name = 'musiccompany_raw_data'
+    local_dir = '/Users/adminstrator/rawdata'
+    
+    # Load encrypted AWS key and AWS secret key
+    with open('AWS_key/aws_key.cfg', 'r') as f:
+        code = f.read()
+    AWS_encrypted_key = code.split('\n')[1].split('=')[1]
+    AWS_encrypted_secret_key = code.split('\n')[2].split('=')[1]
+    AWSAccessKeyId = AWS_encrypted_key
+    AWSSecretKey = AWS_encrypted_secret_key
+    
+    # create an object
+    AWS_connection = ConnectAWS()
+    
+    # connect to AWS
+    asession = AWS_connection.create_session(AWSAccessKeyId, AWSSecretKey)
+    
+    # choose s3 service
+    s3_session = AWS_connection.choose_AWS_service(asession, 's3')
+    
+    # create a bucket, the bucket name is musiccompany_raw_data
+    AWS_connection.create_bucket(s3_session, bucket_name):
+    
+    # upload local data to cloud data lake
+    AWS_connection.upload_file_S3(s3_session, bucket_name, local_dir)
+
+    
+if __name__ == '__main__':
+    main()
