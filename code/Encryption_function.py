8ecefa62befce13161c52b6a18dec6585788623d
kk <kk>
2020-03-21 07:25:40 +0800

    STYLE: Add blank line under packages

--- code/Encryption_function.py
+++ code/Encryption_function.py
@@ -3,6 +3,7 @@ from simplecrypt import encrypt, decrypt
 from base64 import b64encode, b64decode
 from getpass import getpass  
 	
+	
 class AWSEncryption():    
 	def encrypt_AWSKEY(self, key_content):
 		"""
